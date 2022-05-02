932c2c247c0c6cf9e8230ecf89f0db997f412439
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-02 17:12:36 +0430

    use canvas2d

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod point;
 mod cubic_curve;
+mod canvas2d;
 use point::Point;
+use canvas2d::Canvas2d;
 
 fn main() {
 	let p = Point(1.0, 2.0, 3.0);
 	println!("{}", p.0);
+
+	let canvas = Canvas2d::new(1080, 1920);
 }

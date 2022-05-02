6ba74bd1efa3ead25ea22a7857cfbbf2d110a589
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-02 17:12:42 +0430

    initial impl

---
+++ src/canvas2d.rs
@@ -0,0 +1,13 @@
+pub struct Canvas2d {
+  pub width: u32,
+  pub height: u32,
+}
+
+impl Canvas2d {
+  pub fn new(width: u32, height: u32) -> Canvas2d {
+    Canvas2d {
+      width,
+      height,
+    }
+  }
+}

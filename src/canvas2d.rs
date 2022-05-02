93029bf69ce572d8f40d1dbecafb01310044d48d
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-02 17:23:36 +0430

    implement add function and draw drawables in render function

--- src/canvas2d.rs
+++ src/canvas2d.rs
@@ -1,6 +1,9 @@
+use crate::drawable::Drawable;
+
 pub struct Canvas2d {
   pub width: u32,
   pub height: u32,
+  drawable_objects: Vec<Box<dyn Drawable>>,
 }
 
 impl Canvas2d {
@@ -8,11 +11,19 @@ impl Canvas2d {
     Canvas2d {
       width,
       height,
+      drawable_objects: Vec::new(),
     }
+  }
+
+  pub fn add(&mut self, drawable: Box<dyn Drawable>) {
+    self.drawable_objects.push(drawable);
   }
 
   pub fn render(&self) {
     println!("{}", self.height);
     println!("{}", self.width);
+    self.drawable_objects.iter().for_each(
+      |drawable| drawable.draw(self)
+    );
   }
 }

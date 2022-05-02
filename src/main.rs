1dc0095e7bd73266540ba28c29a01f86b2cd9ff0
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-02 17:28:29 +0430

    use cubic curve

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ mod drawable;
 use point::Point;
 use canvas2d::Canvas2d;
 
+use crate::cubic_curve::CubicCurve;
+
 fn main() {
 	let p = Point(1.0, 2.0, 3.0);
 	println!("{}", p.0);
 
-	let canvas = Canvas2d::new(1080, 1920);
+	let curve = CubicCurve (
+		Point(0.0, 0.0, 0.0),
+		Point(1.0, 1.0, 1.0),
+		Point(2.0, 2.0, 2.0),
+		Point(3.0, 3.0, 3.0),
+	);
+
+	let mut canvas = Canvas2d::new(1080, 1920);
+	canvas.add(Box::new(curve));
 	canvas.render();
 }

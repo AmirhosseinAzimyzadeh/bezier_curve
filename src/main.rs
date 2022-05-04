2010366cc1c2f953c898bdc6f4a0754b0e7f4a6d
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-04 16:39:29 +0430

    chang use of point

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use canvas2d::Canvas2d;
 use crate::cubic_curve::CubicCurve;
 
 fn main() {
-	let p = Point(1.0, 2.0, 3.0);
+	let p = Point(1, 2);
 	println!("{}", p.0);
 
 	let curve = CubicCurve (
-		Point(0.0, 0.0, 0.0),
-		Point(1.0, 1.0, 1.0),
-		Point(2.0, 2.0, 2.0),
-		Point(3.0, 3.0, 3.0),
+		Point(0, 0),
+		Point(1, 1),
+		Point(2, 2),
+		Point(3, 3),
 	);
 
 	let mut canvas = Canvas2d::new(1080, 1920);
 	canvas.add(Box::new(curve));
-	canvas.render();
+	// canvas.render();
 }

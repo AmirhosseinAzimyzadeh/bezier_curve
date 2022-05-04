40efe847a52bddc20fc5ea6ee7d66b94377e65cf
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-04 16:38:55 +0430

    change structure to 2d (remove third number)

--- src/point.rs
+++ src/point.rs
@@ -1,1 +1,14 @@
-pub struct Point(pub f32, pub f32, pub f32);
+use crate::drawable::Drawable;
+use crate::canvas2d::Canvas2d;
+
+pub struct Point(pub u32, pub u32);
+
+impl Drawable for Point {
+  fn draw(&self, canvas: &Canvas2d) {
+    // check if point is inside canvas
+    if self.0 > canvas.width && self.1 > canvas.height {
+      return;
+    }
+    
+  }
+}

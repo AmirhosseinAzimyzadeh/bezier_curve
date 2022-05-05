9700c65f4ceedef5e90b3226ec239750ee133051
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-05 11:57:15 +0430

    impl ment get point at t

--- src/cubic_curve.rs
+++ src/cubic_curve.rs
@@ -13,3 +13,17 @@ impl Hittable for CubicCurve {
     return true;
   }
 }
+
+impl CubicCurve {
+  pub fn get_point_at(&self, t: f32) -> Point {
+    let one_minus_t = 1.0 - t;
+    let one_minus_t_2 = one_minus_t * one_minus_t;
+    let one_minus_t_3 = one_minus_t_2 * one_minus_t;
+
+    let new_point = self.0.clone() * one_minus_t_3 +
+      self.1.clone() * 3.0 * one_minus_t_2 * t +
+      self.2.clone() * 3.0 * one_minus_t * t * t +
+      self.3.clone() * t * t * t;
+    Point(new_point.0, new_point.1)
+  }
+}

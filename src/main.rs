ed646a5c55374b0791d7dbe7ead34e0b6a8fc1e6
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-06 06:59:47 +0430

    change number types

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use canvas2d::Canvas2d;
 use crate::cubic_curve::CubicCurve;
 
 fn main() {
-	let p = Point(1, 2);
+	let p = Point(1.0, 2.0);
 	println!("{}", p.0);
 
 	let curve = CubicCurve (
-		Point(0, 0),
-		Point(1, 1),
-		Point(2, 2),
-		Point(3, 3),
+		Point(0.0, 100.0),
+		Point(0.0, 0.0),
+		Point(100.0, 100.0),
+		Point(100.0, 0.0),
 	);
 
-	let mut canvas = Canvas2d::new(1080, 1920);
-	canvas.add(Box::new(curve));
+	let mut canvas = Canvas2d::new(100, 100);
+	canvas.add(curve);
 	canvas.render();
 }

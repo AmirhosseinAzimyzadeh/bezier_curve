8616ed24310deacfb939d7e94d69dda9a020928f
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2022-05-14 19:24:43 +0430

    add comments  + remove unnecessary codes

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ use canvas2d::Canvas2d;
 use crate::cubic_curve::CubicCurve;
 
 fn main() {
-	let p = Point(1.0, 2.0);
-	println!("{}", p.0);
-
+	// canvas size
 	let width = 200.0;
 	let height = 200.0;
 
+	// define cubic curve
 	let curve = CubicCurve (
 		Point(0.0, 0.0),
 		Point(0.0, width - 100.0),
@@ -21,7 +20,11 @@ fn main() {
 		Point(height - 100.0, width - 100.0),
 	);
 
+	// create canvas
 	let mut canvas = Canvas2d::new(width as u32, height as u32);
+	
+	// add curve to canvas
 	canvas.add(curve);
-	canvas.render();
+
+	canvas.render(); // output.ppm
 }
